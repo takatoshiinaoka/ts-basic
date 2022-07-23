d0af26327d5d4b30415b03cca5c79bb52d0d8391
rice-hill <heavenhayao2316@gmail.com>
2022-07-23 16:58:07 +0900

    [add] anything

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,16 +5,16 @@ module.exports = {
     },
     extends: [
         "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "prettier",
+        "plugin:@typescript-eslint/recommended", // TypeScriptでチェックされる項目をLintから除外する設定
+        "prettier", // prettierのextendsは他のextendsより後に記述する
         "prettier/@typescript-eslint",
     ],
-    plugin: ["@typescript-eslint"],
+    plugins: ["@typescript-eslint"],
     parser: "@typescript-eslint/parser",
     parserOptions: {
         "sourceType": "module",
-        "project": "./tsconfig.json"
+        "project": "./tsconfig.json" // TypeScriptのLint時に参照するconfigファイルを指定
     },
-    root: true,
+    root: true, // 上位ディレクトリにある他のeslintrcを参照しないようにする
     rules: {}
 }
