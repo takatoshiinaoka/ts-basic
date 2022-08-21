7ccdf7b78f8efd7a1e5d975c0003f21737b1ab3c
takatoshiinaoka <heavenhayao2316@gmail.com>
2022-08-21 12:50:32 +0900

    [add]index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
-import World from './world'
+// import World from './world'
 
-const root: HTMLElement | null = document.getElementById('root')
-const world = new World('Hello World!!!')
-world.sayHello(root)
+// const root: HTMLElement | null = document.getElementById('root')
+// const world = new World('Hello World!!!')
+// world.sayHello(root)
+
+// 03.基本の型定義
+import { primitiveSample, notExistSample } from './basic'
+
+notExistSample()
+
+primitiveSample()
