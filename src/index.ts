a9f0190bb7d59186895c70c13f177b804006ab96
takatoshiinaoka <heavenhayao2316@gmail.com>
2022-08-21 14:44:08 +0900

    [add] function types

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,24 @@
 // import World from './world'
-
 // const root: HTMLElement | null = document.getElementById('root')
 // const world = new World('Hello World!!!')
 // world.sayHello(root)
 
 // 03.基本の型定義
-import { primitiveSample, notExistSample, anySample, unknownSanple } from './basic'
+// import { primitiveSample, notExistSample, anySample, unknownSanple } from './basic'
+// unknownSanple()
+// anySample()
+// notExistSample()
+// primitiveSample()
 
-unknownSanple()
+// 04.関数の型定義
+import { logMessage, LogMessage6, LogMessage7 } from './function/basic'
+import { isUserSignedIn, isUserSignedIn2, sumProductsPrice } from './function/parameter'
 
-anySample()
-
-notExistSample()
-
-primitiveSample()
+logMessage('Hello TypeScript!')
+LogMessage6('message6')
+LogMessage7('message7')
+isUserSignedIn('ABC', 'takatoshi')
+isUserSignedIn('DEF')
+isUserSignedIn2('ABC')
+const sum = sumProductsPrice(100, 200, 300, 400, 500)
+console.log('Functon parameters sapmple 5:', sum)
