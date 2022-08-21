02e4ca1f839c8f6bc0c919a4a93d8c36b8a4b281
takatoshiinaoka <heavenhayao2316@gmail.com>
2022-08-21 19:30:49 +0900

    [add] array types

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,6 @@
 // 04.関数の型定義
 // import { logMessage, LogMessage6, LogMessage7 } from './function/basic'
 // import { isUserSignedIn, isUserSignedIn2, sumProductsPrice } from './function/parameter'
-
 // logMessage('Hello TypeScript!')
 // LogMessage6('message6')
 // LogMessage7('message7')
@@ -24,8 +23,13 @@
 // console.log('Functon parameters sapmple 5:', sum)
 
 // 05.オブジェクトの型定義
-import objectSample from './object/object'
-import typeAliasSample from './object/alias'
+// import objectSample from './object/object'
+// import typeAliasSample from './object/alias'
+// objectSample()
+// typeAliasSample()
 
-objectSample()
-typeAliasSample()
+// 06.配列とタプルの型定義
+import arraySample from './array/array'
+import tupleSample from './array/tuple'
+arraySample()
+tupleSample()
