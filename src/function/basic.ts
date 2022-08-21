6f4ccad8fd2d00c12271478985265864a7b4ad27
takatoshiinaoka <heavenhayao2316@gmail.com>
2022-08-21 14:12:13 +0900

    [add] tab

--- src/function/basic.ts
+++ src/function/basic.ts
@@ -3,19 +3,19 @@ export const logMessage4 = (message: string): void => console.log('Function basi
 
 // アロー関数
 export const logMessage = (message: string): void => {
-  console.log('Function basic sample 1:', message)
+    console.log('Function basic sample 1:', message)
 }
 
 // 名前付き関数
 export function logMessage2(message: string): void {
-  console.log('Function basic sample 2:', message)
+    console.log('Function basic sample 2:', message)
 }
 
 // 関数式
 export const logMessage3 = function (message: string): void {
-  console.log('Function basic sample 3:', message)
+    console.log('Function basic sample 3:', message)
 }
 
 export const alwaysThrowError = (message: string): never => {
-  throw new Error(message)
+    throw new Error(message)
 }
